79a177667e4505d6900683717553c3bb5c6f6c9c
Kanishk Dudeja <kanishk@kanishkdudeja.com>
2025-10-17 19:59:16 +0530

    separate config for ghostty on macos

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -3,5 +3,14 @@
 # Make a directory to store Ghostty config
 mkdir -p ~/.config/ghostty/
 
-# Symlink the config file
-ln -sf ~/dotfiles/ghostty/config ~/.config/ghostty/config
+# Symlink MacOS's config file
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    # Symlink the config file
+    ln -sf ~/dotfiles/ghostty/config-mac ~/.config/ghostty/config
+fi
+
+# Linux (Includes WSL)
+if [[ "$(uname -s)" == "Linux" ]]; then
+    # Symlink the config file
+    ln -sf ~/dotfiles/ghostty/config-linux ~/.config/ghostty/config
+fi
