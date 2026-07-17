7cebb219e2def9d6eef038685e1c7cdaf8b01665
Kanishk Dudeja <kanishk@kanishkdudeja.com>
2026-07-17 18:36:51 +0530

    set up tmux sensible

--- tmux/install.sh
+++ tmux/install.sh
@@ -9,4 +9,13 @@ fi
 echo "Creating symlink for Tmux configuration file"
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
+# Install TPM (Tmux Plugin Manager)
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  echo "TPM not installed. Installing..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Install/update plugins declared in .tmux.conf
+~/.tmux/plugins/tpm/bin/install_plugins
+
 echo "TMUX configuration finished"
